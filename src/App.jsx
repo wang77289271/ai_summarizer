2a531b0ae43e4d1558328b3202bf3eb4ab9bf106
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-02-24 16:43:38 -0700

    add demo & hero section,  redux toolkit & articleApi from repidAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import './App.css'
+import Demo from './components/Demo'
+import Hero from './components/Hero'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  )
 }
 
 export default App
