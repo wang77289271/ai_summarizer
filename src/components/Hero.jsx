eca793f17efa488ac7643abb78300cb6edfcb7fd
Ye (Shawn) Wang <wang77289271@gmail.com>
2024-04-02 13:12:11 -0600

    change imgs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-16 object-contain' />
+        <img src={logo} alt='sumz_logo' className='w-36 object-contain' />
         <button
           className='black_btn'
           type='button'
